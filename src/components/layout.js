512ccb53fd23c2132f8f3b887d396e054585b8f6
Voronko <unglory44@gmail.com>
2019-10-28 14:31:34 +0300

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,11 +8,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Layout, Menu, Button, Icon } from "antd"
+import { Link } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
+import "antd/dist/antd.css"
+const { SubMenu } = Menu
+const { Header, Content, Sider } = Layout
 
-const Layout = ({ children }) => {
+const AppLayout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,22 +29,51 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Header siteTitle={data.site.siteMetadata.title} className="header">
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          selectable={false}
+          style={{ lineHeight: "64px" }}
+        >
+          <Menu.Item key="1">
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              Главная
+            </Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link
+              to="/new-post"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              Новая запись
+            </Link>
+          </Menu.Item>
+        </Menu>
+
+        <div>
+          <Button type="primary" size="large">
+            Войти
+          </Button>
+        </div>
+      </Header>
+
+      <Content style={{ padding: "0 50px" }}>
+        <Layout style={{ padding: "24px 0", background: "#fff" }}>
+          <Content style={{ padding: "0 24px", minHeight: 280 }}>
+            {children}
+          </Content>
+        </Layout>
+      </Content>
     </>
   )
 }
@@ -49,4 +82,4 @@ Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
-export default Layout
+export default AppLayout
